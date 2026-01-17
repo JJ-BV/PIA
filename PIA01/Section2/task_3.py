71d6e506d1acc1758bcffa9dafbf4c06ff9b723f
Juan Jesús Bonilla <juan.bonilla@the-cocktail.com>
2026-01-17 12:05:06 +0100

    task_3

--- PIA01/Section2/task_3.py
+++ PIA01/Section2/task_3.py
@@ -5,3 +5,33 @@
 # 2. La unión de ambos conjuntos (todos los elementos sin duplicados).
 # 3. La diferencia simétrica (elementos que están en uno u otro conjunto,
 # pero no en ambos).
+
+
+def myFunction (listA, listB):
+    # 1. La intersección de ambos conjuntos (elementos comunes).
+    # elementos comunes de A y B
+    listABIntersection = listA.intersection(listB) 
+    # 2. La unión de ambos conjuntos (todos los elementos sin duplicados).
+    listABUnion = listA.union(listB)
+    # 3. La diferencia simétrica (elementos que están en uno u otro conjunto, pero no en ambos).
+    # diferencia simétrica: elementos de A que no están en B
+    listDifAB = listA.difference(listB)
+    # diferencia simétrica: elementos de B que no están en A
+    listDifBA = listB.difference(listA)
+
+    result = {
+        'intersection' : listABIntersection,
+        'union': listABUnion,
+        'simetricDiff': {
+            'AB': sorted(listDifAB),
+            'BA': sorted(listDifBA)
+        }
+        }
+    for k, v in result.items():
+        print(f"{k} --> {v}")
+
+# dos formar de crear conjuntos
+listA = set([0,1,2,3,4,5])
+listB = {0,4,5,6,7,8,9}
+
+myFunction(listA, listB)
